19df39e21b2119e0c591fc7842ed2a82f1391b18
Busna27 <lucabusnelli2001@gmail.com>
2022-03-03 12:16:14 +0100

    do with function

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,10 +1,13 @@
 const casualNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (let i = 0; i < casualNumbers.length; i++) {
-    if(casualNumbers[i]%2 == 0) {
-        document.getElementById("color-red").innerHTML += casualNumbers[i];
-    } else {
-        document.getElementById("color-green").innerHTML += casualNumbers[i];
+function redOrGreen(arrayNumber) {
+    for (let i = 0; i < arrayNumber.length; i++) {
+        if(arrayNumber[i]%2 == 0) {
+            document.getElementById("color-red").innerHTML += arrayNumber[i];
+        } else {
+            document.getElementById("color-green").innerHTML += arrayNumber[i];
+        }
     }
 }
 
+redOrGreen(casualNumbers);
