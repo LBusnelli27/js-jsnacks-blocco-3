197cef37fe7534ae0bf99bd68fa7b6e594aed770
Busna27 <lucabusnelli2001@gmail.com>
2022-03-03 12:50:26 +0100

    add comment function

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,3 +1,9 @@
+/**
+ * Function that sum the 2 numbers and tell if the result iss odd or even
+ * 
+ * @param {*} firstNum Put the first number
+ * @param {*} secondNum Put the second number
+ */
 function sumOdd(firstNum, secondNum) {
     if((firstNum + secondNum)%2 == 0) {
         console.log(`La somma dei numeri: pari`);
