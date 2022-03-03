f06b1c8e2fc3ca6a619849331058cc86343f9ff3
Busna27 <lucabusnelli2001@gmail.com>
2022-03-03 12:41:32 +0100

    js comments for the function

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,5 +1,10 @@
 const casualNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+/**
+ * Split the odd number in one div and the even number in another div
+ * 
+ * @param {*} arrayNumber Put the array of number
+ */
 function redOrGreen(arrayNumber) {
     for (let i = 0; i < arrayNumber.length; i++) {
         if(arrayNumber[i]%2 == 0) {
